e6d9f9c07cc7925209ccec9dd3f9d4aed591fa26
MOMIJI05 <127726670+MOMIJI05@users.noreply.github.com>
2025-04-24 03:31:39 +0800

    Update Form.java

--- src/Form.java
+++ src/Form.java
@@ -1,8 +1,15 @@
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 
 public class Form {
     @FXML
@@ -24,8 +31,24 @@ public class Form {
     @FXML
     private Label submitMessageLabel;
 
-    public void submitButtonOnAction(ActionEvent event){
-        submitMessageLabel.setText("請至少選擇1個類型");
+    public void submitButtonOnAction(ActionEvent event) throws IOException{
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        FXMLLoader recommendLoader = new FXMLLoader();
+        recommendLoader.setLocation(getClass().getResource("Recommend.fxml"));
+        Scene recommendScene = new Scene(recommendLoader.load());
+        String recommendCSS = this.getClass().getResource("Recommend.css").toExternalForm();
+        recommendScene.getStylesheets().add(recommendCSS);
+
+        if(vaildSubmit()){
+            stage.setScene(recommendScene);
+        }
+        else{
+            submitMessageLabel.setText("請至少選擇1個類型");
+        }
+    }
+
+    public boolean vaildSubmit(){
+        return true;
     }
 
 }

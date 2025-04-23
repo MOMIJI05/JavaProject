8c77612468bc4a23199e018b13a6a3bb0b0e82c8
MOMIJI05 <127726670+MOMIJI05@users.noreply.github.com>
2025-04-24 03:29:43 +0800

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -10,31 +9,18 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("Login.fxml"));
-            //loader.setLocation(getClass().getResource("Form.fxml"));
-            //loader.setLocation(getClass().getResource("Recommend.fxml"));
-            //loader.setLocation(getClass().getResource("Introduction.fxml"));
-            //loader.setLocation(getClass().getResource("Favorities.fxml"));
-            //loader.setLocation(getClass().getResource("DeleteAccount.fxml"));
-            //loader.setLocation(getClass().getResource("Logout.fxml"));
+            FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
+            Scene loginScene = new Scene(loginLoader.load());
+            String loginCSS = this.getClass().getResource("Login.css").toExternalForm();
+            loginScene.getStylesheets().add(loginCSS);
 
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-
-            String css = this.getClass().getResource("Login.css").toExternalForm();
-            //String css = this.getClass().getResource("Form.css").toExternalForm();
-            //String css = this.getClass().getResource("Recommend.css").toExternalForm();
-            //String css = this.getClass().getResource("Introduction.css").toExternalForm();
-            //String css = this.getClass().getResource("Favorities.css").toExternalForm();
-            //String css = this.getClass().getResource("DeleteAccount.css").toExternalForm();
-            //String css = this.getClass().getResource("Logout.css").toExternalForm();
-            scene.getStylesheets().add(css);
+            ////////////////////////////////////////////////////////////
+            
+            Image icon = new Image("file:\\c:\\Users\\Addmin\\Desktop\\JavaProject\\resource\\stackOverflowIcon.png");
 
             primaryStage.setTitle("自動化電影推薦");
-            Image icon = new Image("file:\\c:\\Users\\Addmin\\Desktop\\JavaProject\\resource\\stackOverflowIcon.png");
             primaryStage.getIcons().add(icon);
-            primaryStage.setScene(scene);
+            primaryStage.setScene(loginScene);
             primaryStage.setWidth(1280);
             primaryStage.setHeight(800);
             primaryStage.show();

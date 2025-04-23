83756e67d9b79667415d33b64b587e925553d9f1
MOMIJI05 <127726670+MOMIJI05@users.noreply.github.com>
2025-04-24 03:33:50 +0800

    Update Recommend.java

--- src/Recommend.java
+++ src/Recommend.java
@@ -1,8 +1,13 @@
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class Recommend {
     @FXML
@@ -22,9 +27,34 @@ public class Recommend {
     @FXML
     private Button submitButton;
     @FXML
-    private Label submitMessageLabel;
+    private Button accountButton;
+    @FXML
+    private Label recommendMessageLabel;
+
 
     public void submitButtonOnAction(ActionEvent event){
-        submitMessageLabel.setText("請至少選擇1個類型");
+        recommendMessageLabel.setText("請至少選擇1個類型");
+    }
+
+    public void accountButtonOnAction(ActionEvent event) throws IOException{
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        FXMLLoader favoriteLoader = new FXMLLoader(getClass().getResource("Favorite.fxml"));
+        Scene favoriteScene = new Scene(favoriteLoader.load());
+        String favoriteCSS = this.getClass().getResource("Favorite.css").toExternalForm();
+        favoriteScene.getStylesheets().add(favoriteCSS);
+
+        stage.setScene(favoriteScene);
+    }
+
+    public void movieButton1OnAction(ActionEvent event) throws IOException{
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        FXMLLoader introductionLoader = new FXMLLoader(getClass().getResource("Introduction.fxml"));
+        Scene introductionScene = new Scene(introductionLoader.load());
+        String introductionCSS = this.getClass().getResource("Introduction.css").toExternalForm();
+        introductionScene.getStylesheets().add(introductionCSS);
+
+        stage.setScene(introductionScene);
     }
 }
